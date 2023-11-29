9f0ef826ba7222a76dde5fc0099f1bc6f185b4b3
Gabriel Reis <gabrieldreisilva4@gmail.com>
2023-11-29 00:30:34 -0300

    Update callgenie.sql

--- pratica1/callgenie.sql
+++ pratica1/callgenie.sql
@@ -80,7 +80,7 @@ CREATE TABLE Faq (
 
 -- Criação do usuário admin, se ainda não existir
 INSERT INTO Administrador (adm_nome, adm_telefone, adm_email, adm_senha)
-VALUES ('admin', '12997881456', 'emaildoadm@callgenie.com', 'admin')
+VALUES ('admin', '12997881456', 'emaildoadm@callgenie.com', 'fatec')
 ON DUPLICATE KEY UPDATE adm_nome=VALUES(adm_nome);
 
 
@@ -179,4 +179,101 @@ VALUES ('Problema com a impressora', 'A minha impressora está com tanque de tin
 VALUES ('Problema com o scanner', 'O scanner da minha impressora não está funcionando', 'Concluido', '2020-09-27', 'urgente', '2020-09-27', '12345678937');
 VALUES ('Problema de bateria SmartWatch', 'Meu Smartwatch está com problema e não carrega', 'Concluido', '2020-09-27', 'urgente', '2020-09-27', '12345678938');
 VALUES ('Problema com o teclado do notebook', 'O teclado do meu notebook não está funcionando', 'Concluido', '2020-09-27', 'urgente', '2020-09-27', '12345678939');
+VALUES ('Problema de Segurança', 'Vírus detectado', 'Concluido', '2020-09-27', 'urgente', '2020-09-27', '12345678940');
+VALUES ('Backup Não Realizado', 'Backup automático não está funcionando','Concluido', '2020-09-27', 'urgente', '2020-09-27', '12345678941');
+VALUES ('Problema de Hardware', 'Hardware defeituoso', 'Aberto', '2020-09-27', 'alta', '2020-09-27', '12345678942');
+VALUES ('Problema com Antivírus', 'Antivírus desativado', 'Em Andamento' ,'2020-09-27', 'media', '2020-09-28' ,'12345678943');
+VALUES ('Erro no Excel', 'Excel trava ao abrir', 'Em Andamento' ,'2020-09-27', 'media', '2020-09-28' ,'12345678944'); 
+VALUES ('Problema de Acesso', 'Não consigo acessar a rede', 'Aberto', '2020-12-16', 'baixa', '2020-12-20', '12345678945');
+VALUES ('Problema de Energia', 'Computador desliga inesperadamente', '2023-12-17 11:00:00', 'Em Andamento', '2023-12-21 14:00:00'),
+VALUES ('Problema com Monitor', 'Monitor piscando', '2023-12-18 16:00:00', 'Concluído', '2023-12-22 12:30:00'),
+VALUES ('Problema de Áudio', 'Sem som no computador', '2023-12-19 14:30:00', 'Aberto', '2023-12-23 10:30:00'),
+VALUES ('Problema de Acesso', 'Usuário com dificuldade de acessar o sistema', '2023-12-02 10:30:00', 'Aberto', '2023-12-04 12:45:00'),
+VALUES ('Problema de Impressão', 'Impressora não está imprimindo corretamente', '2023-12-03 12:45:00', 'Em Andamento', '2023-12-06 15:00:00')
 
+--Inserções Suporte
+INSERT INTO Suporte (sup_cpf, sup_nome, sup_email, sup_telefone, sup_senha)
+VALUES ('12345678951', 'Cristiano', 'suporte1@gmail.com', '12997881456', 'suporte1'),
+    ('12345678952', 'Cristina', 'suporte2@gmail.com', '12997881457', 'suporte2'),
+    ('12345678953', 'Cristiane', 'suporte3@gmail.com', '12997881458', 'suporte3'),
+    ('12345678954', 'Laura', 'suporte4@gmail.com','12997881459', 'suporte4'),
+    ('12345678955', 'Larissa', 'suporte5@gmail.com','12997881460', 'suporte5'),
+    ('12345678956', 'Leticia', 'suporte6@gmail.com','12997881461', 'suporte6'),
+    ('12345678957', 'Luis', 'suporte7@gmail.com','12997881462', 'suporte7'),
+    ('12345678958', 'Thaís', 'suporte8@gmail.com','12997881463', 'suporte8'),
+    ('12345678959', 'Juliana', 'suporte9@gmail.com','12997881464', 'suporte9'),
+    ('12345678960', 'Julieta', 'suporte10@gmail.com','12997881465', 'suporte10'),
+    ('12345678961', 'Romeu', 'suporte11@gmail.com','12997881466', 'suporte11'),
+    ('12345678962', 'Ricardo', 'suporte12@gmail.com','12997881467', 'suporte12'),
+    ('12345678963', 'Alice', 'suporte13@gmail.com', '12997881468', 'suporte13');
+
+--Inserções FAQ
+INSERT INTO FAQ (faq_id, faq_pergunta, faq_resposta, faq_sup_id)
+VALUES
+ ('Como posso redefinir minha senha?', 'Você pode redefinir sua senha 
+acessando a página de recuperação de senha em nosso site e seguindo as 
+instruções fornecidas.',1),
+ ('Qual é o horário de atendimento?', 'Nosso horário de atendimento é das 
+9h às 18h, de segunda a sexta-feira. Estamos fechados nos fins de semana e 
+feriados.',2),
+ ('Como posso abrir um novo chamado de suporte?', 'Para abrir um novo 
+chamado de suporte, faça login em sua conta e vá para a seção de suporte. Clique
+em "Abrir Chamado" e preencha o formulário com os detalhes do problema.',4),
+ ('Existe um número de telefone de suporte?', 'Sim, nosso número de suporte
+é (XX) XXXX-XXXX. Estamos disponíveis durante o horário comercial para ajudar 
+com suas dúvidas e problemas.',8),
+ ('Como verificar o status do meu chamado?', 'Você pode verificar o status 
+do seu chamado fazendo login em sua conta e indo para a seção de suporte. Lá, 
+você encontrará uma lista de seus chamados com os status correspondentes.',12),
+ ('Posso cancelar um chamado após aberto?', 'Infelizmente, uma vez que um 
+chamado é aberto, não é possível cancelá-lo. Recomendamos fornecer o máximo de 
+detalhes possível ao abrir um chamado para uma resolução mais rápida.',3),
+ ('Qual é o tempo médio de resposta?', 'Nosso objetivo é responder a todos 
+os chamados no prazo de 24 horas úteis. No entanto, o tempo real pode variar 
+dependendo da complexidade do problema e da carga de suporte.',1),
+ ('Posso adicionar mais informações ao meu chamado?', 'Sim, você pode 
+adicionar mais informações ao seu chamado respondendo ao e-mail de confirmação 
+que recebeu. Isso ajudará nossa equipe a entender melhor a situação e fornecer 
+uma solução mais rápida.',7),
+ ('Como posso atualizar minhas informações de contato?', 'Para atualizar 
+suas informações de contato, faça login em sua conta e vá para as configurações 
+de perfil. Lá, você encontrará opções para editar suas informações, incluindo email e número de telefone.',10),
+ ('O que fazer se minha pergunta não estiver aqui?', 'Se sua pergunta não 
+estiver na lista, entre em contato conosco através do formulário de suporte ou 
+do número fornecido, e ficaremos felizes em ajudar.',3),
+ ('Como faço para instalar a última atualização do software?', 'Para 
+instalar a última atualização do software, visite nossa página de downloads e 
+siga as instruções fornecidas para a versão mais recente do produto.',6),
+ ('Posso transferir minha licença para outro dispositivo?', 'Sim, você pode
+transferir sua licença para outro dispositivo. Entre em contato conosco e 
+forneceremos as instruções necessárias para realizar a transferência.',11),
+ ('Qual é a política de reembolso?', 'Nossa política de reembolso permite 
+reembolsos dentro dos primeiros 30 dias após a compra. Certifique-se de ler 
+nossos termos e condições para obter informações detalhadas.', 9),
+ ('Como faço para relatar um bug no software?', 'Para relatar um bug no 
+software, vá para a seção de suporte em nosso site e selecione a opção "Relatar 
+Bug". Descreva o problema detalhadamente para que nossa equipe possa 
+investigar.',5),
+ ('Onde posso encontrar tutoriais sobre o uso do software?', 'Oferecemos 
+tutoriais em vídeo e documentação detalhada em nossa página de suporte. Explore 
+esses recursos para obter ajuda na instalação e no uso do software.', 2);
+
+--Inserções RespostaChamado
+
+--Inserções Equipamento
+INSERT INTO Equipamento (equ_nome, equ_numserie, equ_tipo, equ_cham_id, equ_sup_id)
+VALUES('Notebook para Chamado 1', '1254ZX5FG001', 'nootebook' 1, 2),
+ ('Desktop para Chamado 4', '1254ZX5FG002', 'nootebook' 4, 8),
+ ('Notebook para Chamado 7', '1254ZX5FG003', 'nootebook' 7, 10),
+ ('Desktop para Chamado 14', '1254ZX5FG004', 'nootebook' 14, 6),
+ ('Notebook para Chamado 16', '1254ZX5FG005', 'nootebook' 16, 9),
+ ('Desktop para Chamado 19', '1254ZX5FG006', 'nootebook' 19, 12),
+ ('Notebook para Chamado 21', '1254ZX5FG007', 'nootebook' 21, 3),
+ ('Desktop para Chamado 24', '1254ZX5FG008', 'nootebook' 24, 1),
+ ('Notebook para Chamado 31', '1254ZX5FG009', 'nootebook' 31, 11),
+ ('Desktop para Chamado 33', '1254ZX5FG010', 'nootebook' 33, 7),
+ ('Notebook para Chamado 38', '1254ZX5FG011', 'nootebook' 38, 5),
+ ('Desktop para Chamado 41', '1254ZX5FG012', 'nootebook' 41, 4),
+ ('Notebook para Chamado 44', '1254ZX5FG013', 'nootebook' 44, 2),
+ ('Desktop para Chamado 47', '1254ZX5FG014', 'nootebook' 47, 8),
+ ('Notebook para Chamado 50', '1254ZX5FG015', 'nootebook' 50, 1);
